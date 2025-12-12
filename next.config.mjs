e8b48335aa77d5ac5051605953770e6c3933f901
Kazuo-Mikara <htoomyatkyaw32@gmail.com>
2025-12-12 14:06:56 +0630

    Edit next.js config for Netlify Deployment

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,11 @@ const nextConfig = {
     endpoint: process.env.VITE_APPWRITE_ENDPOINT,
     projectId: process.env.VITE_APPWRITE_PROJECT_ID
   },
+  experimental: {
+    useDeploymentId: true,
+    // Optionally, use with Server Actions
+    useDeploymentIdServerActions: true,
+  },
 };
 
 export default nextConfig;
